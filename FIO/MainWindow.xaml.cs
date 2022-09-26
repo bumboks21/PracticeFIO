7a70342271b1a9b1113a7769c6372f87d7767b07
bumboks21 <113061095+bumboks21@users.noreply.github.com>
2022-09-26 09:45:24 +0300

    first commit

---
+++ FIO/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FIO
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        User _user;
+        private ObservableCollection<User> _users;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _users= new ObservableCollection<User>();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName = firstNameTextBox.Text;
+            string middleName = middleNameTextBox.Text;
+            string lastName = lastNameTextBox.Text;
+            _user = new User(firstName, middleName, lastName);
+            _users.Add(_user);
+            listView.ItemsSource = _users;
+            listView.DisplayMemberPath = "FIO";
+
+        }
+    }
+}
---
+++ FIO/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIO
+{
+    public class User
+    {
+        
+
+        public string FIO { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public User(string firstName, string middleName, string lastName)
+        {
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
+            FIO = firstName + " " + middleName[0] + "." + lastName[0] + ".";
+        }
+    } 
+}
